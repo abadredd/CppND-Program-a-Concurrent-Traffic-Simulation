700205480d094bbafcc576dd126f972c7ee80709
abadredd <49213550+abadredd@users.noreply.github.com>
2024-09-08 15:55:20 -0400

    Update TrafficLight.h

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -19,9 +19,13 @@ template <class T>
 class MessageQueue
 {
 public:
-
-private:
+	T receive();
+    void send(T &&msg);
     
+private:
+    std::mutex _mutex;
+    std::condition_variable _condition;
+    std::deque<T> _queue;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
@@ -30,21 +34,36 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+enum TrafficLightPhase {
+    red,
+    green
+};
+
+class TrafficLight : public TrafficObject
 {
 public:
     // constructor / desctructor
+    TrafficLight();
 
     // getters / setters
-
     // typical behaviour methods
+
+    void waitForGreen();
+    void simulate();
+
+    TrafficLightPhase getCurrentPhase();
 
 private:
     // typical behaviour methods
+    void cycleThroughPhases();
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
+
+    TrafficLightPhase _currentPhase;
+    MessageQueue<TrafficLightPhase> _messageQueue; 
+
 
     std::condition_variable _condition;
     std::mutex _mutex;
